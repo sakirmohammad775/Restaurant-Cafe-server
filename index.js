d933db2ceffa79cc9a3f18d87db79053dbfc5733
Sakir Mohammad <sakirmohammad775@gmail.com>
2024-04-27 22:03:41 +0600

    delete fumctionality added

--- index.js
+++ index.js
@@ -50,7 +50,13 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
-    
+    //delete functionality
+    app.delete('users/:id',async(req,res)=>{
+      const id =req.params.id
+      const query={_id:new ObjectId(id)}
+      const result=await userCollection.deleteOne(query)
+      res.send(result)
+    })
 
     //menu related api
     app.get('/menu',async(req,res)=>{
